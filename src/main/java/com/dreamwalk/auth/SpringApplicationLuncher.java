d274891ca68c9bf3a814a04f0a71d22c0f1fa099
St1ng3rB0ng <tolikmalovf@gmail.com>
2025-11-22 19:27:44 +0200

    Initial commit

---
+++ src/main/java/com/dreamwalk/auth/SpringApplicationLuncher.java
@@ -0,0 +1,11 @@
+package com.dreamwalk.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApplicationLuncher {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringApplicationLuncher.class, args);
+    }
+}
